4e1008364f69660b15b0555afa5dcd4e653ebb47
chenbojian <chenbojian@foxmail.com>
2017-12-12 22:26:14 +0800

    finish base64 problem

--- Base64-Encoding/index.js
+++ Base64-Encoding/index.js
@@ -1,31 +1,62 @@
+const numToB64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const b64ToNum = [...numToB64].reduce((acc, cur, idx) => {
+    acc[cur] = idx;
+    return acc;
+}, {});
 
-function toBase64() {
-    const bitResult = [...this]
-    .map(c => {
-        let num = c.charCodeAt(0);
-        let bitArray = [];
-        for(let i=0;i<8;i++) {
-            let bit = num %2;
-            num = Math.floor(num / 2);
-            bitArray.splice(0, 0, bit);
-        }
-        return bitArray;
-    })
-    .reduce((acc, cur) => acc.concat(cur),[]);
-    let base64Array = [];
-    for(let i = 0; i < bitResult.length; i++) {
-        if (i % 6 === 0) {
-            base64Array.push(bitResult[i]);
-            continue;
-        }
-        base64Array[base64Array.length - 1] = base64Array[base64Array.length - 1] << 1 + bitResult[i];
+function toBinary(num, bit) {
+    const b = num.toString(2);
+    if (b.length > bit) {
+        throw new Error('not support');
     }
-    console.log(base64Array)
-    return base64Array.map(i => String.fromCharCode(i)).join('');
+    return Array(bit - b.length).fill(0).join('') + b;
 }
 
-function fromBase64(str) {
+function _toBase64(chars) {
+    if (chars.length > 3) {
+        throw new Error();
+    }
 
+    let bitArray = [...chars].map(c => toBinary(c.charCodeAt(0), 8)).join('');
+    if (bitArray.length < 24) {
+        bitArray += Array(24 - bitArray.length).fill(0).join('');
+    }
+    let arr = Array(1 + chars.length).fill().map((n, idx) => idx);
+    let equals = Array(3 - chars.length).fill('=').join('');
+
+    return arr.map(i => numToB64[parseInt(bitArray.slice(i * 6, (i + 1) * 6), 2)]).join('') + equals;
+}
+
+function _fromBase64(chars) {
+    if (chars.length !== 4) {
+        throw new Error();
+    }
+    let bitArray = [...chars].filter(c => c !== '=').map(c => toBinary(b64ToNum[c], 6)).join('');
+
+    const count = Math.floor(bitArray.length / 8);
+    let arr = Array(count).fill().map((n, idx) => idx);
+
+    return arr.map(i => parseInt(bitArray.slice(i * 8, (i + 1) * 8), 2)).map(i => String.fromCharCode(i)).join('');
+}
+
+function splitChars(chars, count) {
+    return [...chars]
+        .reduce((acc, cur, idx) => {
+            if (idx % count === 0) {
+                return [...acc, [cur]];
+            }
+            acc[acc.length - 1].push(cur);
+            return acc;
+        }, []);
+}
+
+
+function toBase64() {
+    return splitChars(this, 3).map(_toBase64).join('');
+}
+
+function fromBase64() {
+    return splitChars(this, 4).map(_fromBase64).join('');
 }
 
 
--- Base64-Encoding/test.js
+++ Base64-Encoding/test.js
@@ -3,4 +3,8 @@ require('./index');
 it('should convert from/to base64', () => {
     expect('this is a string!!'.toBase64()).toEqual('dGhpcyBpcyBhIHN0cmluZyEh');
     expect('dGhpcyBpcyBhIHN0cmluZyEh'.fromBase64()).toEqual('this is a string!!');
+    expect('any carnal pleasure.'.toBase64()).toEqual('YW55IGNhcm5hbCBwbGVhc3VyZS4=');
+    expect('any carnal pleasure.').toEqual('YW55IGNhcm5hbCBwbGVhc3VyZS4='.fromBase64());
+    expect('any carnal pleasure'.toBase64()).toEqual('YW55IGNhcm5hbCBwbGVhc3VyZQ==');
+    expect('any carnal pleasure').toEqual('YW55IGNhcm5hbCBwbGVhc3VyZQ=='.fromBase64());
 });
