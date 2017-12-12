43c6096eb9ddda7fa0ea620457085093d6b5e8b3
chenbojian <chenbojian@foxmail.com>
2017-12-12 23:45:12 +0800

    fix typo

--- Point-In-Polygon/index.js
+++ Point-In-Polygon/index.js
@@ -4,8 +4,8 @@ module.exports = function pointInPoly(poly, point) {
         const p1 = poly[i];
         const p2 = poly[(i + 1) % poly.length];
 
-        const v1 = [p1[0] - point[0], p1[1], point[1]];
-        const v2 = [p2[0] - point[0], p2[1], point[1]];
+        const v1 = [p1[0] - point[0], p1[1] - point[1]];
+        const v2 = [p2[0] - point[0], p2[1] - point[1]];
         const cosine = (v1[0] * v2[0] + v1[1] * v2[1]) / (Math.sqrt(v1[0] * v1[0] + v1[1] * v1[1]) * Math.sqrt(v2[0] * v2[0] + v2[1] * v2[1]));
         const alpha = Math.acos(cosine);
         angles.push(alpha);
